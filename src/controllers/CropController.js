f790027d7f111d57a4b07c6c798264607c6e3376
Chibucious <ezukachibueze@gmail.com>
2023-02-08 12:06:32 +0100

    Made corrctions to crop and input controler

--- src/controllers/CropController.js
+++ src/controllers/CropController.js
@@ -1,7 +1,7 @@
 //Import validation result
 const { validationResult } = require('express-validator');
 const crypto = require('crypto');
-const { Crop, CropSpecification, CropRequest, ErrorLog, User, Category, Auction, SubCategory } = require('~database/models');
+const { Crop, CropSpecification, CropRequest, ErrorLog, User, Category, Auction, SubCategory, Bid } = require('~database/models');
 const md5 = require('md5');
 var appRoot = require('app-root-path');
 
@@ -74,12 +74,6 @@ class CropController {
                     sampleFile.mv(uploadPath, function (err) {
                         if (err) {
                             return res.status(500).send(err + " Error in uploading file");
-                        } else {
-
-                            // res.send('File uploaded!');
-
-                            // image = "image"+i;
-                            // my_object.image = "uploadPath"
                         }
                     });
                 }
@@ -193,7 +187,7 @@ class CropController {
             if (logError) {
                 return res.status(500).json({
                     error: true,
-                    message: 'Unable to complete request at the moment'+e.toString()
+                    message: 'Unable to complete request at the moment'
                 })
             }
         }
@@ -246,8 +240,7 @@ class CropController {
                     as: 'user',
                 }],
                 where: { type: "wanted", active: 1 },
-                order: [['id', 'DESC']],
-                group: ["id"]
+                order: [['id', 'DESC']]
             });
 
 
@@ -306,8 +299,7 @@ class CropController {
                 }],
 
                 where: { type: "auction", active: 1 },
-                order: [['id', 'DESC']],
-                group: ["id"]
+                order: [['id', 'DESC']]
             });
 
 
@@ -374,8 +366,7 @@ class CropController {
                         as: 'user',
                     }],
                     where: { type: "offer", active: 1 },
-                    order: [['id', 'DESC']],
-                    group: ["id"]
+                    order: [['id', 'DESC']]
                 });
 
 
@@ -435,9 +426,8 @@ class CropController {
                 },
                 ],
 
-                where: { user_id: req.global.user.id, active: 1 },
-                order: [['id', 'DESC']],
-                group: ["id"]
+                where: { user_id: req.body.user_id, active: 1 },
+                order: [['id', 'DESC']]
             });
 
 
@@ -497,11 +487,14 @@ class CropController {
                     }, {
                         model: User,
                         as: 'user'
+                    },
+                    {
+                        model: Auction,
+                        as: "auction"
                     }],
 
                     where: { id: cropId },
-                    order: [['id', 'DESC']],
-                    group: ["id"]
+                    order: [['id', 'DESC']]
                 });
 
                 return res.status(200).json({
@@ -533,10 +526,113 @@ class CropController {
     }
     /* --------------------------- GET CROP BY ID --------------------------- */
 
+    static async getCropBids(req, res) {
+        try {
+            /* ------------------------ UPDATE INTO CROP TABLE ----------------------- */
+
+            var bids = await Bid.findAll({ where: { crop_id: req.params.id } });
+            if (bids) {
+
+                return res.status(200).json({
+                    error: false,
+                    message: "Bids returned successfully",
+                    data: bids
+                })
+
+            } else {
+                return res.status(200).json({
+                    error: false,
+                    message: "No bids on this crop",
+                    data: []
+                })
+            }
+
+        } catch (e) {
+            var logError = await ErrorLog.create({
+                error_name: "Error on edit a crop",
+                error_description: e.toString(),
+                route: "/api/crop/:id/bid",
+                error_code: "500"
+            });
+            if (logError) {
+                return res.status(500).json({
+                    error: true,
+                    message: 'Unable to complete request at the moment'
+                })
+            }
+        }
 
 
+    }
 
 
+    static async bidForCrop(req, res) {
+
+        const errors = validationResult(req);
+
+        try {
+
+            if (!errors.isEmpty()) {
+                // return res.status(400).json({ errors: errors.array() });
+                return res.status(200).json({
+                    "error": true,
+                    "message": "All fields are required",
+                    "data": errors
+                })
+            }
+
+            /* ------------------------ UPDATE INTO CROP TABLE ----------------------- */
+
+            var crop = await Crop.findOne({ where: { id: req.params.id } });
+            if (crop) {
+
+                //Check for existing bid
+                var existingBid = await Bid.findOne({
+                    where: { user_id: req.body.user_id, crop_id: req.params.id }
+                });
+
+                if (!existingBid) {
+                    var bid = await Bid.create({
+                        user_id: req.body.user_id,
+                        crop_id: req.params.id,
+                        amount: req.body.amount
+                    });
+
+                    return res.status(200).json({
+                        error: false,
+                        message: "Bid created",
+                    });
+
+                } else {
+                    return res.status(400).json({
+                        error: true,
+                        message: "You have already bidded on this product",
+                    })
+                }
+
+            } else {
+                return res.status(400).json({
+                    error: true,
+                    message: "No such crop found",
+                    data: req.body
+                })
+            }
+
+        } catch (e) {
+            var logError = await ErrorLog.create({
+                error_name: "Error on edit a crop",
+                error_description: e.toString(),
+                route: "/api/crop/:id/bid",
+                error_code: "500"
+            });
+            if (logError) {
+                return res.status(500).json({
+                    error: true,
+                    message: 'Unable to complete request at the moment'
+                })
+            }
+        }
+    }
     /* --------------------------- GET ALL CROPS TYPE BY USERID --------------------------- */
     static async getByTypeandUserID(req, res) {
 
@@ -551,25 +647,12 @@ class CropController {
                     as: "category"
                 },
                 {
-                    model: SubCategory,
-                    as: "subcategory"
-                },
-                {
-                    model: User,
-                    as: "user"
-                },
-                {
-                    model: SubCategory,
-                    as: "subcategory"
-                },
-                {
                     model: Auction,
                     as: "auction"
                 }],
 
                 where: { type: req.params.type, user_id: req.params.userid },
-                order: [['id', 'DESC']],
-                group: ["id"]
+                order: [['id', 'DESC']]
             });
 
 
