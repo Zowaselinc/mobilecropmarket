825f63bd4a6266f37b72ce514a81fd3e5d1c6a54
Chibucious <ezukachibueze@gmail.com>
2023-02-17 15:52:38 +0100

    See error message in add Crop controller

--- src/controllers/CropController.js
+++ src/controllers/CropController.js
@@ -176,7 +176,7 @@ class CropController {
             if (logError) {
                 return res.status(500).json({
                     error: true,
-                    message: 'Unable to complete request at the moment '+e.toString()
+                    message: 'Unable to complete request at the moment rfnbjb '+e.toString()
                 })
             }
         }
