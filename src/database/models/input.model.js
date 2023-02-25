ca1743ba8035afaf7302d7df9fbde0ba47d2d094
Chibucious <ezukachibueze@gmail.com>
2023-02-25 19:20:40 +0100

    Removed state from input model

--- src/database/models/input.model.js
+++ src/database/models/input.model.js
@@ -84,10 +84,6 @@ let Schema = (Sequelize, mode) => {
             type: Sequelize.STRING,
             allowNull: false
         },
-        state: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
         video: {
             type: Sequelize.STRING,
         },
