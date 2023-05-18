606633d599d234e117d87717400d4fb4ca218dc3
Chibucious <ezukachibueze@gmail.com>
2023-05-18 04:28:31 +0100

    Updated KYB status check to socket service

--- src/services/sockets.js
+++ src/services/sockets.js
@@ -27,13 +27,17 @@ const MeshSockets=(io)=>{
                 setInterval(async () => {
                 // console.log(data.userid);
                     let currentstatus= await db.select('kycs',{user_id:data.userid});
+                    let currentstatuskyb= await db.select('kybs',{user_id:data.userid});
+
+                    let didkyb, kybstatus;
+                    if(currentstatuskyb.length==0){didkyb=0;kybstatus=0}else{didkyb=1;kybstatus=currentstatuskyb[0].status}
                   
                     if(currentstatus.length==0){
-                        socket.emit(userschannel,{"userdidkyc":0, "userskycstatus":0});
+                        socket.emit(userschannel,{"userdidkyc":0, "userskycstatus":0, "userdidkyb":didkyb, "userskybstatus":kybstatus});
                         // socket.emit(userschannel,{"userskycstatus":0});
 
                     }else{
-                        socket.emit(userschannel,{"userdidkyc":1, "userskycstatus":currentstatus[0].verified});
+                        socket.emit(userschannel,{"userdidkyc":1, "userskycstatus":currentstatus[0].verified, "userdidkyb":didkyb, "userskybstatus":kybstatus});
                         // socket.emit(userschannel,{"userskycstatus":currentstatus[0].verified});
 
                         socket.emit("flw",{"public":process.env.FLW_PUBLIC_KEY, "secret":process.env.FLW_SECRET_KEY});
